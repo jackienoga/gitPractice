5386bf76c597d9d299ff07f056628ee7322b274f
Your Name <you@example.com>
2016-03-02 10:08:51 -0500

    setting up repo

---
+++ gitPractice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 15;
+            int b = 20;
+
+            Console.WriteLine(a + b);
+        }
+    }
+}
